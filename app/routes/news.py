53ff6e41cef14db3d9195ff118cdf57bf08002da
Pasha <zzzkelrath@yandex.ru>
2025-06-16 15:57:42 +0700

    Added endpoints

--- app/routes/news.py
+++ app/routes/news.py
@@ -3,8 +3,23 @@ from models.news import News
 
 router = APIRouter()
 
-@router.get("/get_news_by_ticker")
+@router.get("/get_all_news/{ticker}")
 async def get_news_by_ticker(ticker: str):
     ...
 
-@router.get("/get_x")
+@router.get("/get_green_news/{ticker}")
+async def get_green_news(ticker: str):
+    ...
+
+@router.get("/get_red_news/{ticker}")
+async def get_red_news(ticker: str):
+    ...
+
+@router.get("/get_news_by_importance/{ticker}")
+async def get_news_by_importance(ticker: str):
+    ...
+
+@router.get("/get_news_by_source/{ticker}/{src}")
+async def get_news_by_source(ticker:str, src: str):
+    ...
+
