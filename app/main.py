0720f1ae46dad8998f29c8feb201cb09789bc115
Pasha <zzzkelrath@yandex.ru>
2025-06-18 03:55:59 +0700

    add news

--- app/main.py
+++ app/main.py
@@ -2,12 +2,22 @@ from fastapi import FastAPI, APIRouter
 from app.routes.auth import router as auth_router
 from contextlib import asynccontextmanager
 from app.models.database import engine, create_tables
+from app.models.ticker import create_initial_tickers, get_tickers
+from app.models.price import load_prices_for_ticker
+from app.models.news import load_initial_news_by_ticker
+from fastapi.middleware.cors import CORSMiddleware
 
 
 @asynccontextmanager
 async def lifespan(app: FastAPI):
     print("Creating tables...")
     await create_tables()
+    await create_initial_tickers()
+    tickers = await get_tickers()
+    for ticker in tickers:
+        await load_prices_for_ticker(ticker=ticker.name, ticker_id=ticker.id)
+        await load_initial_news_by_ticker(ticker_name=ticker.name)
+    await load_prices_for_ticker("GAZP", 2)
     yield
     print("Disposing engine...")
     await engine.dispose()
--- app/models/news.py
+++ app/models/news.py
@@ -1,12 +1,54 @@
-from sqlalchemy import Column, Integer, String, Double, DateTime, func
-
-from app.models.database import Base
+from sqlalchemy import Column, Integer, String, Boolean, DateTime, func, select
+from app.models.database import Base, AsyncSessionLocal
+from datetime import datetime
+import aiofiles
+import json
 
 
 class News(Base):
     __tablename__ = "News"
     id = Column(Integer, primary_key=True, index=True)
-    ticker = Column
+    ticker = Column(String)
     source = Column(String)
     summary_text = Column(String)
-    timestamp = created_at = Column(DateTime, server_default=func.now())
+    is_green = Column(Boolean)
+    created_at = Column(DateTime, server_default=func.now())
+
+
+async def load_initial_news_by_ticker(ticker_name: str):
+    filename = f"news/{ticker_name.lower()}.json"
+    async with AsyncSessionLocal() as session:
+        try:
+            async with aiofiles.open(filename, mode="r", encoding="utf-8") as f:
+                content = await f.read()
+                data = json.loads(content)
+
+            for item in data:
+                existing_news = await session.execute(
+                    select(News).where(
+                        News.summary_text == item["summary_text"],
+                        News.created_at == datetime.fromisoformat(item["timestamp"]),
+                    )
+                )
+                if not existing_news.scalar_one_or_none():
+                    news = News(
+                        ticker=item["ticker"],
+                        source=item["source"],
+                        summary_text=item["summary_text"],
+                        created_at=datetime.fromisoformat(item["timestamp"]),
+                    )
+                    session.add(news)
+
+            await session.commit()
+            return {
+                "status": "success",
+                "message": f"News for {ticker_name} loaded successfully",
+            }
+
+        except FileNotFoundError:
+            return {"status": "error", "message": f"File {filename} not found"}
+        except json.JSONDecodeError:
+            return {"status": "error", "message": f"File {filename} is not valid JSON"}
+        except Exception as e:
+            await session.rollback()
+            return {"status": "error", "message": str(e)}
